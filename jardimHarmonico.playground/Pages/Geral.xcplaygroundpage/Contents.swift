ad5c29df3ee59de7cecfca4f6a6e9012ae9885b4
julianoctvaz@github.com <jctv@cin.ufpe.br>
2020-04-07 22:35:37 -0300

    Melhorando Navegacao entre telas

--- jardimHarmonico.playground/Pages/Geral.xcplaygroundpage/Contents.swift
+++ jardimHarmonico.playground/Pages/Geral.xcplaygroundpage/Contents.swift
@@ -9,26 +9,26 @@ class OnboardingViewController: UIViewController {
     
     //Buttons and Labels
     let buttonToPlay = UIButton()
-     
+    
     override func loadView() { ///Carega/Cria a view
         //setting Buttons
         buttonToPlay.frame = CGRect(x: 1120, y: 820, width: 250, height: 100)
         buttonToPlay.setImage(UIImage(named: "buttons/proximaFaseButtonVerde"), for: .normal)
         
-                       
+        
         //Views, UIImage and ImagenViews
         let view = UIView()
         let background = UIImageView()
-                              
         
-        background.image = UIImage(named: "onboarding")
+        
+        background.image = UIImage(named: "diversos/onboarding")
         background.frame = CGRect(x: 0, y: 0, width: 1440, height: 900)
-                     
+        
         
         //Target Button
-         buttonToPlay.addTarget(self, action: #selector(touchedButton), for: .touchUpInside)
-    
-                
+        buttonToPlay.addTarget(self, action: #selector(touchedButton), for: .touchUpInside)
+        
+        
         //Subviews
         view.addSubview(background)
         view.addSubview(buttonToPlay)
@@ -43,25 +43,78 @@ class OnboardingViewController: UIViewController {
         let novaViewController = LevelOneViewController(screenType: .mac , isPortrait: true)
         navigationController?.pushViewController(novaViewController, animated: true)
         navigationController?.navigationBar.isHidden = true
-         
+        
     }
-          
+    
 }
-
-
-
 
 class LevelOneViewController: UIViewController {
     
-    override func loadView() {  ///Carega/Cria a view
-        //Views and ImageViews
+    //Buttons and Labels
+    let buttonToPlay = UIButton()
+    let buttonToReset = UIButton()
+    let buttonToOnboarding = UIButton()
+    
+    override func loadView() { ///Carega/Cria a view
+        
+        //setting Buttons
+        buttonToPlay.frame = CGRect(x: 1120, y: 820, width: 250, height: 100)
+        buttonToPlay.setImage(UIImage(named: "buttons/proximaFaseButton"), for: .normal)
+        
+        buttonToReset.frame = CGRect(x: 1120, y: 620, width: 250, height: 100)
+        buttonToReset.setImage(UIImage(named: "buttons/resetButton"), for: .normal)
+        
+        buttonToOnboarding.frame = CGRect(x: 1120, y: 720, width: 250, height: 100)
+        buttonToOnboarding.setImage(UIImage(named: "buttons/onboardingButton"), for: .normal)
+        
+        
+        //Views, UIImage and ImagenViews
         let view = UIView()
-        view.backgroundColor = .purple//colocar imagem
+        let background = UIImageView()
+        
+        background.image = UIImage(named: "diversos/background")
+        background.frame = CGRect(x: 0, y: 0, width: 1440, height: 900)
+        
+        
+        
+        //Target Button
+        buttonToReset.addTarget(self, action: #selector(touchedButtonReset), for: .touchUpInside)
+        buttonToOnboarding.addTarget(self, action: #selector(touchedButtonToBackToOnboarding), for: .touchUpInside)
+        //butaoToPlay nao funciona nessa parte ainda
+        
+        
+        //Subviews
+        view.addSubview(background)
+        view.addSubview(buttonToReset)
+        view.addSubview(buttonToPlay)
+        view.addSubview(buttonToOnboarding)
+    
+        
+        
         
         self.view = view
+        
     }
+    
+    //IBActions
+    @IBAction func touchedButtonReset() {
+        print("Apertou botão reset")
+        
+        
+        
+    }
+    
+    @IBAction func touchedButtonToBackToOnboarding() {
+        print("Apertou botão para voltar a OnboardingViewController ")
+        let novaViewController = LevelOneViewController(screenType: .mac , isPortrait: true)
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.popViewController(animated: true)
+        navigationController?.pushViewController(novaViewController, animated: true)
+        
+        
+    }
+    
 }
-/*
 
 class LevelTwoViewController: UIViewController {
     
@@ -69,12 +122,12 @@ class LevelTwoViewController: UIViewController {
         
         
         let view = UIView()
-        view.backgroundColor = .white//colocar imagem
+        view.backgroundColor = .green//colocar imagem
         
         
         let label = UILabel()
         label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World to Level one!"
+        label.text = "Hello World to Level 2!"
         label.textColor = .black
         
         
@@ -82,66 +135,67 @@ class LevelTwoViewController: UIViewController {
         self.view = view
     }
     
-    override func viewDidLoad() {}///chamada quando a view, criada via storyboard, termina de ser carregada
 }
-
-
-class LevelThreeViewController: UIViewController {
-    
-    override func loadView() {  ///Carega/Cria a view
-        
-        
-        let view = UIView()
-        view.backgroundColor = .white//colocar imagem
-        
-        
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World to Level one!"
-        label.textColor = .black
-        
-        
-        view.addSubview(label)
-        self.view = view
-    }
-    
-    override func viewDidLoad() {}///chamada quando a view, criada via storyboard, termina de ser carregada
-}
-
-
-class FinalChallengeViewController: UIViewController {
-    
-    override func loadView() {  ///Carega/Cria a view
-        
-        
-        let view = UIView()
-        view.backgroundColor = .white//colocar imagem
-        
-        
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World to FinalChalenge!"
-        label.textColor = .black
-        
-        
-        view.addSubview(label)
-        self.view = view
-    }
-    
-    override func viewDidLoad() {}///chamada quando a view, criada via storyboard, termina de ser carregada
-}
-
-
-*/
+/*
+ 
+ 
+ class LevelThreeViewController: UIViewController {
+ 
+ override func loadView() {  ///Carega/Cria a view
+ 
+ 
+ let view = UIView()
+ view.backgroundColor = .white//colocar imagem
+ 
+ 
+ let label = UILabel()
+ label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+ label.text = "Hello World to Level one!"
+ label.textColor = .black
+ 
+ 
+ view.addSubview(label)
+ self.view = view
+ }
+ 
+ override func viewDidLoad() {}///chamada quando a view, criada via storyboard, termina de ser carregada
+ }
+ 
+ 
+ class FinalChallengeViewController: UIViewController {
+ 
+ override func loadView() {  ///Carega/Cria a view
+ 
+ 
+ let view = UIView()
+ view.backgroundColor = .white//colocar imagem
+ 
+ 
+ let label = UILabel()
+ label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+ label.text = "Hello World to FinalChalenge!"
+ label.textColor = .black
+ 
+ 
+ view.addSubview(label)
+ self.view = view
+ }
+ 
+ override func viewDidLoad() {}///chamada quando a view, criada via storyboard, termina de ser carregada
+ }
+ 
+ 
+ */
 
 // Present the view controller (vc) in the Live View window
 let onboardingViewController = OnboardingViewController()
 let levelOneViewController = LevelOneViewController()
+let levelTwoViewController = LevelTwoViewController()
 
 let navigation = UINavigationController(screenType: .mac, isPortrait: true)
-    
+
 navigation.pushViewController(onboardingViewController, animated: true)
-    
+
 PlaygroundPage.current.liveView = navigation.scale(to: 0.5)
 
 
