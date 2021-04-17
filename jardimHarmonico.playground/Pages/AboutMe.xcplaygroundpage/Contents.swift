61922d440b4b4c67393728132ff10efc70ecf361
Juliano Cezar Teles Vaz <jctv@cin.ufpe.br>
2021-04-17 17:37:27 -0300

    Update Contents.swift

--- jardimHarmonico.playground/Pages/AboutMe.xcplaygroundpage/Contents.swift
+++ jardimHarmonico.playground/Pages/AboutMe.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 //:Hello, my name is Juliano Cezar Teles Vaz and love music and educational technologies.
 //: Currently I am developer at Apple Developer Academy. Also, I am studying Computer Science at the Informatics Center (CIn)
-//: at the Federal University of Pernambuco (UFPE). This is Harmonic Garden was my fisrt solo project in the Apple Developer Academy - Recife, Brazil.
-//: We using Challenge-based Learning (CBL) methodology and I literally saw my mind blown up when i concreted this idea!
+//: at the Federal University of Pernambuco (UFPE). The Harmonic Garden was my fisrt solo project in the Apple Developer Academy - Recife, Brazil.
+//: We used Challenge-based Learning (CBL) methodology and I literally saw my mind blown up when i concreted this idea!
 //: Specially because we use UIKit, Gesture, AVFoundation, Animation... I hope, have you like it!
 
 /*:
