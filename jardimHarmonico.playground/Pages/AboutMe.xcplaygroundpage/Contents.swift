18866df88ee812251e879a58c9f8d9420b44f62c
Juliano Cezar Teles Vaz <jctv@cin.ufpe.br>
2021-04-17 17:52:42 -0300

    Update Contents.swift

--- jardimHarmonico.playground/Pages/AboutMe.xcplaygroundpage/Contents.swift
+++ jardimHarmonico.playground/Pages/AboutMe.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@
 /*:
 Hello, my name is Juliano Cezar Teles Vaz and love music and educational technologies.
 Currently I am developer at Apple Developer Academy e we have been learning a lof of technologies and frameworks by Apple. Also, I am studying Computer Science at the Informatics Center (CIn) at the Federal University of Pernambuco (UFPE). I chose this project to submit, because it is special to me, last year I had not finished it yet to submit and was my fisrt solo project in the Apple Developer Academy - Recife, Brazil.
-We used Challenge-based Learning (CBL) methodology and I literally saw my mind blown up when i concreted this idea! I show it from anyone with proud! I until made this [article](https://medium.com/academyufpe/sobre-ter-boas-ideias-menos-eureka-e-mais-t%C3%A9cnicas-e-m%C3%A9todos-8e899ffbdda7) in Medium to share my experience about the ideation, prototyping, and development.
+We used Challenge-based Learning (CBL) methodology and I literally saw my mind blown up when i concreted this idea! I show it for anyone with proud! I until made this [article](https://medium.com/academyufpe/sobre-ter-boas-ideias-menos-eureka-e-mais-t%C3%A9cnicas-e-m%C3%A9todos-8e899ffbdda7) in Medium to share my experience about the ideation, prototyping, and development.
 */
 
 /*:
